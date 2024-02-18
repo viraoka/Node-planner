f18fa69ac21aad92c8ce27973fdfc2435c13a6d7
viraoka <139058037+viraoka@users.noreply.github.com>
2024-02-18 18:10:11 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const routes = require('./MealRoutes');
+const cors = require('cors');
+
+
+require('dotenv').config();
+
+mongoose.set("strictQuery", false);
+
+const PORT = 3000 || process.env.port;
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+.connect(process.env.MONGODB_LINK)
+.then(() => console.log('WE WERE CONNECTED TO MONGO'))
+.catch((err) => console.log(err))
+
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`I AM LISTENING ON PORT ${PORT}`)
+})
+
